e67726b228dfed9a5748acbc93846918ef91c6a6
Skn1999 <sknayyar.sk@gmail.com>
2020-05-12 12:27:59 +0530

    Homepage animations done

---
+++ app.js
@@ -0,0 +1,15 @@
+const hero = document.querySelector(".hero");
+const slider = document.querySelector(".slider");
+const logo = document.querySelector(".logo");
+const hamburger = document.querySelector(".hamburger");
+const headline = document.querySelector(".headline");
+
+
+const tl = new TimelineMax();
+
+tl.fromTo(hero, 1, {height: "0%"}, {height: "80%", ease: Power2.easeInOut})
+.fromTo(hero, 1.2, {width: "100%"}, {width: "80%",  ease: Power2.easeInOut })
+.fromTo(slider, 1.2, {x: "-100%"}, {x: "0%",  ease: Power2.easeInOut}, "-=1.2")
+.fromTo(logo, .5, {opacity: 0, x: "5%"}, {opacity: 1, x:"0%",  ease: Power2.easeInOut}, "-=0.5")
+.fromTo(hamburger, .5, {opacity: 0, x: "5%"}, {opacity: 1, x:"0%",  ease: Power2.easeInOut}, "-=0.5")
+.fromTo(headline, 1, { x: "0%"}, { x:"-50%",  ease: Power2.easeInOut}, "-=1");
